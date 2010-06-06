475f54880265d5c767649bd30d77b29f8b550d8d
Adrian Kosmaczewski <akosma@gmail.com>
2010-06-06 08:20:48 -0700

    Fixed the display of the intro movie

--- Classes/AppDelegate/DemoAppDelegate.m
+++ Classes/AppDelegate/DemoAppDelegate.m
@@ -27,26 +27,15 @@
     NSString *path = [[NSBundle mainBundle] pathForResource:@"tunnel" ofType:@"mp4"];
     NSURL *url = [NSURL fileURLWithPath:path];
     self.moviePlayer = [[[MPMoviePlayerController alloc] initWithContentURL:url] autorelease];
-    self.moviePlayer.shouldAutoplay = YES;
-    self.moviePlayer.controlStyle = MPMovieControlModeHidden;
-    self.moviePlayer.scalingMode = MPMovieScalingModeAspectFit;
     
     NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
     [center addObserver:self 
-               selector:@selector(moviePlaybackFinished:) 
-                   name:MPMoviePlayerPlaybackDidFinishNotification
-                 object:self.moviePlayer];
-    [center addObserver:self 
-               selector:@selector(moviePlaybackFinished:) 
-                   name:MPMoviePlayerDidExitFullscreenNotification
-                 object:self.moviePlayer];
-    [center addObserver:self 
-               selector:@selector(moviePlaybackChanged:) 
-                   name:MPMoviePlayerPlaybackStateDidChangeNotification
-                 object:self.moviePlayer];
-    [center addObserver:self 
                selector:@selector(movieReady:) 
                    name:MPMoviePlayerLoadStateDidChangeNotification
+                 object:self.moviePlayer];
+    [center addObserver:self 
+               selector:@selector(moviePlaybackFinished:) 
+                   name:MPMoviePlayerPlaybackDidFinishNotification
                  object:self.moviePlayer];
     
     self.moviePlayer.view.frame = [UIScreen mainScreen].bounds;
@@ -71,23 +60,22 @@
 #pragma mark -
 #pragma mark NSNotification handlers
 
+- (void)movieReady:(NSNotification *)notification
+{
+    if (self.moviePlayer.loadState == 3)
+    {
+        self.moviePlayer.scalingMode = MPMovieScalingModeAspectFit;
+        self.moviePlayer.controlStyle = MPMovieControlModeDefault;
+        [self.moviePlayer play];
+    }
+}
+
 - (void)moviePlaybackFinished:(NSNotification *)notification
 {
     [self.window insertSubview:self.mainMenuController.view belowSubview:self.moviePlayer.view];
     [UIView beginAnimations:nil context:NULL];
     self.moviePlayer.view.alpha = 0.0;
     [UIView commitAnimations];
-}
-
-- (void)moviePlaybackChanged:(NSNotification *)notification
-{
-}
-
-- (void)movieReady:(NSNotification *)notification
-{
-    if (self.moviePlayer.loadState == 3)
-    {
-    }
 }
 
 @end
