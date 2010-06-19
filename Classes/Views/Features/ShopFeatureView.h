1077d908da2dd486c1318afa44e3c1372865f02c
Adrian Kosmaczewski <akosma@gmail.com>
2010-06-19 13:14:18 +0200

    First version of the 'shop' feature

---
+++ Classes/Views/Features/ShopFeatureView.h
@@ -0,0 +1,16 @@
+//
+//  ShopFeatureView.h
+//  Digital 2.0
+//
+//  Created by Adrian on 6/6/10.
+//  Copyright 2010 akosma software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FeatureView.h"
+
+@interface ShopFeatureView : FeatureView
+
+
+
+@end
---
+++ Classes/Views/Features/ShopFeatureView.m
@@ -0,0 +1,21 @@
+//
+//  ShopFeatureView.m
+//  Digital 2.0
+//
+//  Created by Adrian on 6/6/10.
+//  Copyright 2010 akosma software. All rights reserved.
+//
+
+#import "ShopFeatureView.h"
+
+@interface ShopFeatureView ()
+
+@end
+
+
+
+@implementation ShopFeatureView
+
+
+
+@end
